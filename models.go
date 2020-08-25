5aa7426198299fbae8560b35e993c4c431f68329
Preksha-zs <preksha.parvatham@kroger.com>
2020-08-25 12:19:07 +0530

    removing extra uploaded files

--- models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// User schema of the user table
-type FavLoc struct {
-	ID       int64  `json:"id"`
-	Name     string `json:"name"`
-	Lat      float32 `json:"lat"`
-	Long     float32 `json:"long"`
-}
