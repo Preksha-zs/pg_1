e915c90a2955e66a6a6de27aa776c57b2dc65431
Preksha-zs <preksha.parvatham@kroger.com>
2020-09-04 15:10:17 +0530

    service is updated

--- FL1/service/fav_loc/fav_loc_service.go
+++ FL1/service/fav_loc/fav_loc_service.go
@@ -1,37 +1,30 @@
 package fav_loc
 
 import (
+	"fmt"
 	"github.com/Preksha-zs/FL1/models"
-	"github.com/Preksha-zs/FL1/service"
 	"github.com/Preksha-zs/FL1/store"
 )
 
 type Fav_loc_service struct {
-	store               store.Fav_loc
+	store store.Fav_loc
 }
 
-func New(s store.Fav_loc) service.Fav_loc {
+func New(s store.Fav_loc) *Fav_loc_service {
 	return &Fav_loc_service{
 		store: s,
 	}
 }
+func (p *Fav_loc_service) Create(fl *models.Fav_loc) *models.Fav_loc {
+	fmt.Println("aaaaaaaaaaaaaaahhhhhhhhhhhhhhhhh")
 
-func (p *Fav_loc_service) Create(id int64,) int64 {
-	//k := models.Fav_loc{}
-	//if err := p.store.InsertFavLoc(models.Fav_loc); err != nil {
-	//	return nil, err
-	//}
-   // var resp=*models.Fav_loc
-	 var k = p.store.InsertFavLoc(models.Fav_loc{})
-	//if err!= nil {
-//		return nil, err
-	//}
-	//}
+	fmt.Println(fl)
 
-	return k
+	resp := p.store.InsertFavLoc(fl)
+	return resp
 }
 
-func (p *Fav_loc_service) GetByID( id int64) (models.Fav_loc, error) {
+func (p *Fav_loc_service) GetByID(id int64) (models.Fav_loc, error) {
 
 	var resp, err = p.store.GetFavLoc(int64(id))
 	if err != nil {
@@ -40,28 +33,22 @@ func (p *Fav_loc_service) GetByID( id int64) (models.Fav_loc, error) {
 	return resp, nil
 }
 
-func (p *Fav_loc_service)Get() ([]models.Fav_loc, error) {
-	//var f []models.Fav_loc
-	//var k =[]models.Fav_loc{}
-   var f,err=p.store.GetAllFavLoc()
+func (p *Fav_loc_service) Get() ([]models.Fav_loc, error) {
+	var f, err = p.store.GetAllFavLoc()
 	if err != nil {
-			return nil, err
-		}
-	return f,nil
+		return nil, err
+	}
+	return f, nil
 }
 
-func (p *Fav_loc_service)Update(id int64) (int64, error) {
-	//resp:=models.Fav_loc{}
-	 var resp = p.store.UpdateFavLoc(int64(id), models.Fav_loc{})
-//	if err != nil {
-	//	return nil, err
-	//}
+func (p *Fav_loc_service) Update(id int64, fl *models.Fav_loc) (*models.Fav_loc, error) {
+	fmt.Println("im in service update function")
+	//	var fl *models.Fav_loc
+	var resp = p.store.UpdateFavLoc(int64(id), fl)
+	fmt.Println("im in hvkj update function")
 	return resp, nil
 }
-func (p *Fav_loc_service)Delete(id int64) error {
+func (p *Fav_loc_service) Delete(id int64) error {
 	p.store.DeleteFavLoc(int64(id))
-	//if err != nil {
-	//	return err
-	//}
 	return nil
 }
--- FL1/service/interface.go
+++ FL1/service/interface.go
@@ -1,16 +1,13 @@
 package service
 
-import "github.com/Preksha-zs/FL1/models"
+import (
+	"github.com/Preksha-zs/FL1/models"
+)
 
-type Fav_loc interface{
-	Create(id int64,) int64
-	GetByID( id int64) (models.Fav_loc, error)
+type Fav_loc_service interface {
+	Create(loc *models.Fav_loc) *models.Fav_loc
+	GetByID(id int64) (models.Fav_loc, error)
 	Get() ([]models.Fav_loc, error)
-	Update(id int64) (int64, error)
+	Update(id int64, fl *models.Fav_loc) (*models.Fav_loc, error)
 	Delete(id int64) error
-	//Create(id int64) (*models.Fav_loc, error)
-	//GetByID( id int64) (*models.Fav_loc, error)
-	//Get() ([]*models.Fav_loc, error)
-//	Update(id int64) (*models.Fav_loc, error)
-	//Delete(id int64) error
 }
