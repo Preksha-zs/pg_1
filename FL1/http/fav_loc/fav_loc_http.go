a0f3b6a12e4f759968c9d7a117cdac4b5d3f44cd
Preksha-zs <preksha.parvatham@kroger.com>
2020-09-04 15:14:22 +0530

    http  is updated

--- FL1/http/fav_loc/fav_loc_http.go
+++ FL1/http/fav_loc/fav_loc_http.go
@@ -1,42 +1,63 @@
 package fav_loc
 
 import (
-	"github.com/Preksha-zs/FL1/http"
+	"fmt"
 	"github.com/Preksha-zs/FL1/models"
 	"github.com/Preksha-zs/FL1/service"
 	"gitlab.kroger.com/platform/krogo/pkg/krogo"
+	"strconv"
 )
 
-type fav_loc struct {
-	service service.Fav_loc
+type Fav_loc struct {
+	service service.Fav_loc_service
 }
 
-func New(s service.Fav_loc) http.Fav_loc {
-	return &fav_loc{service: s}
+func New(s service.Fav_loc_service) *Fav_loc {
+	return &Fav_loc{service: s}
 }
-func (p *fav_loc) Create(ctx *krogo.Context) (interface{}, error) {
+func (p *Fav_loc) Create(ctx *krogo.Context) (interface{}, error) {
+	fmt.Println("yyyyyyyyyyyyyyyyyyqyqyqyqyyqyqyqyqyyqyqyqyyqyqyqyqy")
 	var req *models.Fav_loc
 	err := ctx.Bind(&req)
 	if err != nil || req == nil {
-		return nil, http.InvalidRequestBody()
+		return nil, nil
 	}
-	resp := p.service.Create(int64(id))
+	resp := p.service.Create(req)
+	fmt.Println("22222222223333333333")
+	//	fmt.Println(resp)
 	return resp, nil
 }
-
-func (p *fav_loc) Delete(ctx *krogo.Context) (interface{}, error) {
+func (p *Fav_loc) Delete(ctx *krogo.Context) (interface{}, error) {
 	ID := ctx.PathParam("id")
-	err := p.service.Delete(int64(ID))
-
+	s, _ := strconv.Atoi(ID)
+	err := p.service.Delete(int64(s))
 	return nil, err
 }
-func (p *fav_loc) Read(ctx *krogo.Context) (interface{}, error) {
+func (p *Fav_loc) Update(ctx *krogo.Context) (interface{}, error) {
+	var (
+		resp *models.Fav_loc
+		err  error
+	)
+	ID := ctx.PathParam("id")
+	s, _ := strconv.Atoi(ID)
+	var fl *models.Fav_loc
+	fmt.Println("im in http update function")
+	resp, _ = p.service.Update(int64(s), fl)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("im in http update function")
+	return resp, nil
+}
+func (p *Fav_loc) Read(ctx *krogo.Context) (interface{}, error) {
+	//fmt.Println("%v\n", ctx)
 	var (
 		resp models.Fav_loc
 		err  error
 	)
 	ID := ctx.PathParam("id")
-	resp, err = p.service.GetByID(int64(ID))
+	s, _ := strconv.Atoi(ID)
+	resp, err = p.service.GetByID(int64(s))
 	if err != nil {
 		return nil, err
 	}
