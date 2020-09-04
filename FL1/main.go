ab5b09e7c7a789e3bd6ef86b4002a35b7875aad3
Preksha-zs <preksha.parvatham@kroger.com>
2020-09-04 15:07:09 +0530

    main.go is updated

--- FL1/main.go
+++ FL1/main.go
@@ -8,38 +8,33 @@ import (
 	pst "github.com/Preksha-zs/FL1/store/fav_loc"
 	_ "github.com/lib/pq"
 	"gitlab.kroger.com/platform/krogo/pkg/krogo"
-	"os"
+	//"os"
 )
 
 func createConnection() *sql.DB {
-
-	// Open the connection
 	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=test_db sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
-
 	if err != nil {
 		panic(err)
 	}
-
-	// check the connection
 	err = db.Ping()
-
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
-
 	fmt.Println("Successfully connected!")
-	// return the connection
 	return db
 }
 func main() {
 	k := krogo.New()
-	k.Server.HTTP.Port = 9090
+	k.Server.HTTP.Port = 9091
 	db := createConnection()
 	store := pst.New(db)
 	service := ps.New(store)
 	Handler := ph.New(service)
+	fmt.Printf("%v\n", Handler)
 	k.POST("/FL1", Handler.Create)
+	k.PUT("/FL1/{id}", Handler.Update)
 	k.GET("/FL1/{id}", Handler.Read)
 	k.DELETE("/FL1/{id}", Handler.Delete)
 	k.Start()
